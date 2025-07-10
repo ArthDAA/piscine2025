d8e39a719b3457cba613cc5241370e78e8648199
ArthDAA <arthus.daa06@gmail.com>
2025-07-10 14:32:36 +0200

    mabite

--- C01/ex02/ft_swap.c
+++ C01/ex02/ft_swap.c
@@ -6,9 +6,16 @@
 /*   By: arde-ass <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 19:37:51 by arde-ass          #+#    #+#             */
-/*   Updated: 2025/07/09 19:40:21 by arde-ass         ###   ########.fr       */
+/*   Updated: 2025/07/10 14:29:32 by arde-ass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
 
 void	ft_swap(int *a, int *b)
 {
@@ -18,3 +25,15 @@ void	ft_swap(int *a, int *b)
 	*a = *b;
 	*b = temp;
 }
+
+int	main()
+{
+	int	*ptr;
+	int	*var;
+
+	&int = 5;
+
+	ft_putchar('0' + ptr);
+	ft_swap(*ptr, *var);
+}
+
