383b9683b161f6991cb1c446ad621590c75f4236
ArthDAA <arthus.daa06@gmail.com>
2025-07-13 16:40:27 +0200

    pas fini enft

---
+++ rush00/bonus/ultimate_rush00/main.c
@@ -0,0 +1,34 @@
+void	rush(int x, int y, char *character);
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	char	character[6];
+	
+	if (argc <= 4)
+	{
+		ft_putstr("6 arguments svp!\nFormat: ./a.out A B C D E F\n");
+		return (1);
+	}
+	character[0] = argv[1][0];
+	character[1] = argv[2][0];
+	character[2] = argv[3][0];
+	character[3] = argv[4][0];
+	character[4] = argv[5][0];
+	character[5] = argv[6][0];
+	rush(23, 42, character);
+	return (0);
+}
---
+++ rush00/bonus/ultimate_rush00/rush0X.c
@@ -0,0 +1,50 @@
+typedef struct pos
+{
+	int	x;
+	int	y;
+} t_pos;
+
+void	ft_putchar(char c);
+
+void	ft_print(t_pos p, int i, int j, char *character)
+{
+	if (i == 0 && j == 0)
+		ft_putchar(character[0]);
+	else if (i == 0 && j == p.y - 1)
+		ft_putchar(character[1]);
+	else if (i == p.x - 1 && j == 0)
+		ft_putchar(character[2]);
+	else if (i == p.x - 1 && j == p.y - 1)
+		ft_putchar(character[3]);
+	else if (i == 0 || i == p.x - 1)
+		ft_putchar(character[4]);
+	else if (j == 0 || j == p.y - 1)
+		ft_putchar(character[5]);
+	else
+		ft_putchar(' ');
+}
+
+void	rush(int x, int y, char *character)
+{
+	int	i;
+	int	j;
+	t_pos	p;
+
+	i = 0;
+	j = 0;
+	p.x = x;
+	p.y = y;
+	if (p.x <= 0 || p.y <= 0)
+		return ;
+	while (i < x)
+	{
+		while (j < p.y)
+		{
+			ft_print(p, i, j, character);
+			j++;
+		}
+		ft_putchar('\n');
+		j = 0;
+		i++;
+	}
+}
