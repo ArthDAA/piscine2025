f056685f3203b154cf1f36ef21435402f1a63766
ArthDAA <arthus.daa06@gmail.com>
2025-07-13 06:46:16 +0200

    rush00 fini

--- rush00/bonus/ultimate_rush/main.c
+++ rush00/bonus/ultimate_rush/main.c
@@ -2,7 +2,12 @@ void    rush(int x, int y, char a, char b, char c, char d, char e, char f);
 
 int	main(int argc, char **argv)
 {
-    rush(123, 42, argv[1][0], argv[2][0], argv[3][0], 
-    argv[4][0], argv[5][0], argv[6][0]);
-    return (0);
+	if (argc != 7)
+	{
+		write(1, "6 arguments svp!", 16);
+		return (1);
+	}
+	rush(23, 42, argv[1][0], argv[2][0], argv[3][0], 
+	argv[4][0], argv[5][0], argv[6][0]);
+	return (0);
 }
